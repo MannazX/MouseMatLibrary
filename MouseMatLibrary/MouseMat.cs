7ed953b17c9ac9e521587faf4cdeefe03cda6617
Magnus Sverdrup <magger254@gmail.com>
2025-10-23 21:45:10 +0200

    Add project files.

---
+++ MouseMatLibrary/MouseMat.cs
@@ -0,0 +1,29 @@
+﻿namespace MouseMatLibrary
+{
+	public class MouseMat
+	{
+		public int Id { get; set; }
+		public string Brand { get; set; }
+		public string Material { get; set; }
+		public double Length { get; set; }
+		public double Width { get; set; }
+		public string Color { get; set; }
+		public double Price { get; set; }
+
+		public MouseMat(string brand, string material, double length, double width, string color, double price)
+		{
+			// Include Exceptions here
+			Brand = brand;
+			Material = material;
+			Length = length;
+			Width = width;
+			Color = color;
+			Price = price;
+		}
+
+		public override string ToString()
+		{
+			return $"Brand: {Brand}, Material: {Material}, Length: {Length}, Width: {Width}, Color: {Color}, Price: {Price}";
+		}
+	}
+}

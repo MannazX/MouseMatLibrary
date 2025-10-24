5900663961ebe421d1d6eec77a29dff5377285a3
Magnus Sverdrup <magger254@gmail.com>
2025-10-24 09:55:41 +0200

    Included Test Methods for Objects Instanciation and Repositories

---
+++ MouseMatTests/MouseMatRepositoryTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MouseMatTests
+{
+	[TestClass]
+	public sealed class MouseMatRepositoryTests
+	{
+		[TestMethod]
+		public void TestGet()
+		{
+
+		}
+
+		[TestMethod]
+		public void TestGetById()
+		{
+
+		}
+
+		[TestMethod]
+		public void TestAdd()
+		{
+
+		}
+
+		[TestMethod]
+		public void TestDelete()
+		{
+
+		}
+
+		[TestMethod]
+		public void TestUpdate()
+		{
+
+		}
+	}
+}
---
+++ MouseMatTests/MouseMatTest.cs
@@ -0,0 +1,42 @@
+﻿namespace MouseMatTests
+{
+	[TestClass]
+	public sealed class MouseMatTest
+	{
+		[TestMethod]
+		public void TestBrand()
+		{
+
+		}
+
+		[TestMethod]
+		public void TestMaterial()
+		{
+
+		}
+
+		[TestMethod]
+		public void TestLength()
+		{
+
+		}
+
+		[TestMethod]
+		public void TestWidth()
+		{
+
+		}
+
+		[TestMethod]
+		public void TestColor()
+		{
+
+		}
+
+		[TestMethod]
+		public void TestPrice()
+		{
+
+		}
+	}
+}
